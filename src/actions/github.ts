637a28606b296cde0691a730e9c4716541ae851d
aikin <1@aikin.me>
2018-07-29 00:37:41 +0800

    🔬 tech(TEST): setup test

--- src/actions/github.ts
+++ src/actions/github.ts
@@ -4,17 +4,15 @@ import * as ora from 'ora'
 
 const githubApi: string = 'https://api.github.com'
 
-export const fetchRepos = (username: string) => {
-	;(async () => {
-		try {
-			const spinner = ora('Fetching repos...').start()
-			let response = await axios.get(`${githubApi}/users/${username}/repos`)
-			spinner.clear()
-			spinner.stop()
-			console.log(chalk.blueBright('==============='))
-			console.log(`Repos size: ${chalk.greenBright(response.data.length)}`)
-		} catch (error) {
-			console.log(error)
-		}
-	})()
-}
+export const fetchRepos = (username: string) => {(async () => {
+	try {
+		const spinner = ora('Fetching repos...').start()
+		let response = await axios.get(`${githubApi}/users/${username}/repos`)
+		spinner.clear()
+		spinner.stop()
+		console.log(chalk.blueBright('==============='))
+		console.log(`Repos size: ${chalk.greenBright(response.data.length)}`)
+	} catch (error) {
+		console.log(error)
+	}
+})()}
