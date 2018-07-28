6dd79c00ad7602a59dd3967d479690dfb1209d7e
aikin <1@aikin.me>
2018-07-28 22:39:13 +0800

    🌟 feat(COMMAND): setup command line happy path

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,20 @@
-#!/usr/bin/env node
+import * as commander from 'commander';
+import chalk from 'chalk';
 
+commander
+	.version('1.0.0')
+	.description('A command line seed');
 
+commander
+	.command('try')
+	.alias('t')
+	.description('Just have try')
+	.action(() => {
+		console.log(chalk.yellow('=========*** Try Command Line***=========='));
+	});
+
+if (!process.argv.slice(2).length/* || !/[arudl]/.test(process.argv.slice(2))*/) {
+	commander.outputHelp();
+	process.exit();
+}
+commander.parse(process.argv);
